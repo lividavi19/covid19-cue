fcd8c65d5c0f86c07267d78030a97b493d558909
Livingstone <43893458+lividavi19@users.noreply.github.com>
2020-08-29 17:51:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,58 +1,51 @@
-'use strict';
-
-// cache name
-const STATIC_CACHE_NAME = 'covid19cue-static-cache-v1';
-
-// files to pre-cache
-const FILES_TO_PRECATCHE = [
-	'offline.html',
-	'img/icons/icon_256.png'
+const STATIC_VERSION = 1;
+const DYNAMIC_VERSION = 1;
+const STATIC_CACHE = `static-${STATIC_VERSION}://covid19cue.netlify.app`;
+const DYNAMIC_CACHE = `dynamic-${DYNAMIC_VERSION}://covid19cue.netlify.app`;
+const APP_SHELL = [
+	`.`,
+	`index.html`,
+	`countries.json`,
+	`css/base.css`,
+	`js/base.js`,
+	`js/AsyncRequest.js`,
+	`img/icons/ic_192.png`,
+	`img/icons/ic_512.png`
 ];
 
-// install the service worker
-self.addEventListener('install', evt => {
-	//console.log('service worker has been installed');
-	evt.waitUntil(
-		caches.open(STATIC_CACHE_NAME)
-			.then(cache => {
-				console.log('pre-caching offline assets');
-				cache.addAll(FILES_TO_PRECATCHE);
-			})
+// install event
+self.oninstall = e => {
+	e.waitUntil(
+		caches.open(STATIC_CACHE).then(cache => {
+			cache.addAll(APP_SHELL);
+		})
 	);
-	self.skipWaiting();
-});
+};
 
-// listens for the activate event
-self.addEventListener('activate', evt => {
-	//console.log('service worker has been activated');
-	evt.waitUntil(
-		caches.keys()
-			.then(keys => {
-				//console.log(keys);
-				return Promise.all(keys
-					.filter(key => key !== STATIC_CACHE_NAME)
-					.map(key => caches.delete(key))
-				);
-			})
+// activate event
+self.onactivate = e => {
+	e.waitUntil(
+		caches.keys().then(keys => {
+			return Promise.all(keys
+				.filter(key => key !== STATIC_CACHE & key !== DYNAMIC_CACHE)
+				.map(key => caches.delete(key))
+			);
+		})
 	);
-	self.clients.claim();
-});
+};
 
-// listens for fetch event
-self.addEventListener('fetch', evt => {
-	//console.log('fetch event', evt);
-	if (evt.request.mode !== 'navigate') {
-		// not a page navigation, bail
-		return;
-	}
-
-	evt.respondWith(
-		fetch(evt.request)
-			.catch(() => {
-				return caches.open(STATIC_CACHE_NAME)
-					.then((cache) => {
-						return cache.match('offline.html');
-					});
-			})
+// fetch event
+self.onfetch = e => {
+	e.respondWith(
+		caches.match(e.request).then(cachedResponse => {
+			return cachedResponse || fetch(e.request).then(fetchResponse => {
+				return caches.open(`${DYNAMIC_CACHE}`).then(cache => {
+					// if (e.request.url.lastIndexOf(`.php`)===-1) {
+					// 	cache.put(e.request.url, fetchResponse.clone());
+					// }
+					return fetchResponse;
+				});
+			});
+		})
 	);
-});
+};
